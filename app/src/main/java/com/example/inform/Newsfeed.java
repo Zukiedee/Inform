a24cc81b1ffd9b89c9f00e9abaa3caa07e2b936e
Zukiedee <zukiswalobola26@gmail.com>
2019-08-06 10:50:09 +0200

    All bugs fixed on App. Floating action button to create a notice is commented out.

--- app/src/main/java/com/example/inform/Newsfeed.java
+++ app/src/main/java/com/example/inform/Newsfeed.java
@@ -30,8 +30,8 @@ public class Newsfeed extends AppCompatActivity
         setContentView(R.layout.activity_newsfeed);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        //FloatingActionButton fab = findViewById(R.id.fab);
+        /*fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
@@ -44,7 +44,7 @@ public class Newsfeed extends AppCompatActivity
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
-        navigationView.setNavigationItemSelectedListener(this);
+        navigationView.setNavigationItemSelectedListener(this);       */
     }
 
     @Override
