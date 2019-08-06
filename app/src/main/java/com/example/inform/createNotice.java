c5aa24cb1c6723cabc4fe4c23ee39171621b3fa4
Zukiedee <Zukiswalobola26@gmail.com>
2019-08-06 07:09:07 +0200

    Welcome, Login, Newsfeed, Create Notice GUI's developed.

---
+++ app/src/main/java/com/example/inform/createNotice.java
@@ -0,0 +1,14 @@
+package com.example.inform;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class createNotice extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_notice);
+    }
+}
---
+++ app/src/main/java/com/example/inform/data/Login.java
@@ -0,0 +1,5 @@
+package com.example.inform.data;
+
+public class Login{
+
+}
