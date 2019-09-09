214da0aed8b97e159a1c8269475b3635c91ccb8b
Zukiswa Lobola <Zukiswalobola26@gmail.com>
2019-09-09 11:30:53 +0200

    Update RequestsAdmin.java

--- app/src/main/java/com/communityapp/inform/view/RequestsAdmin.java
+++ app/src/main/java/com/communityapp/inform/view/RequestsAdmin.java
@@ -1,5 +1,6 @@
 package com.communityapp.inform.view;
 
+import android.annotation.SuppressLint;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
@@ -22,21 +23,30 @@ import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Query;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Objects;
 
+/**
+ * Notice requests received to admin from users in the relevant community regarding will be displayed here
+ * Admin reviews the notice request and either accepts or rejects the notice.
+ * Admin then deletes the notice from requests by swipe-to-delete function.
+ */
 public class RequestsAdmin extends AppCompatActivity {
-
     private FirebaseAuth mAuth; //Firebase authentication
     private FirebaseFirestore database = FirebaseFirestore.getInstance();
     private CollectionReference requests = database.collection("Requests");
     private RequestAdapter adapter;
     private ProgressDialog progressDialog;
     private RelativeLayout relativeLayout;
+    private RecyclerView requestsRecyclerView;
+    private String community;
 
     private static final String TITLE_KEY = "Title";
     private static final String CATEGORY_KEY = "Category";
@@ -47,7 +57,6 @@ public class RequestsAdmin extends AppCompatActivity {
     private static final String COMMUNITY_KEY = "Community";
     private static final String USERNAME_KEY = "Username";
     private static final String UID_KEY = "User ID";
-
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -61,62 +70,13 @@ public class RequestsAdmin extends AppCompatActivity {
 
         progressDialog =  new ProgressDialog(this);
         relativeLayout = findViewById(R.id.inbox);
+        requestsRecyclerView = findViewById(R.id.inbox_recyclerView);
 
         checkUserStatus();
 
-        String community ="UCT";
+        community="";
         loadRequests(community);
-
-        adapter.setOnItemClickListener(new RequestAdapter.OnItemClickListener() {
-            @Override
-            public void acceptBtnClick(DocumentSnapshot documentSnapshot, int position) {
-
-                String category = documentSnapshot.getString(CATEGORY_KEY);
-                String DatetoString = documentSnapshot.getString(DATE_KEY);
-                String body = documentSnapshot.getString(DESCRIPTION_KEY);
-                String timeStamp = documentSnapshot.getString(ID_KEY);
-                String image = documentSnapshot.getString(IMAGE_KEY);
-                String title = documentSnapshot.getString(TITLE_KEY);
-                String name = documentSnapshot.getString(USERNAME_KEY);
-                String communities = documentSnapshot.getString(COMMUNITY_KEY);
-                String email = documentSnapshot.getString(UID_KEY);
-                CollectionReference notifications = database.collection("Users/"+email+"/Messages");
-
-                HashMap<String, String> hashMap = new HashMap<>();
-                hashMap.put(CATEGORY_KEY, category);
-                hashMap.put(DATE_KEY, DatetoString);
-                hashMap.put(DESCRIPTION_KEY, body);
-                hashMap.put(ID_KEY, timeStamp);
-                hashMap.put(IMAGE_KEY, image);
-                hashMap.put(TITLE_KEY, title);
-                hashMap.put(USERNAME_KEY, name);
-                hashMap.put(COMMUNITY_KEY, communities);
-                hashMap.put(UID_KEY, email);
-
-                CollectionReference notices = database.collection("Notices");
-                notices.document(Objects.requireNonNull(timeStamp)).set(hashMap)
-                        .addOnSuccessListener(aVoid -> {
-                            HashMap<String, String> msg = feedback(title, DatetoString, timeStamp);
-                            notifications.document(timeStamp).set(msg);
-                            Toast.makeText(RequestsAdmin.this, "Notice posted to newsfeed.\nFeedback sent to user", Toast.LENGTH_LONG).show();
-                        })
-                        .addOnFailureListener(e -> Toast.makeText(RequestsAdmin.this, "Error: "+e.getMessage(), Toast.LENGTH_LONG).show());
-            }
-
-            @Override
-            public void rejectBtnClick(DocumentSnapshot documentSnapshot, int position) {
-
-                String DatetoString = documentSnapshot.getString(DATE_KEY);
-                String timeStamp = documentSnapshot.getString(ID_KEY);
-                String title = documentSnapshot.getString(TITLE_KEY);
-                String email = documentSnapshot.getString(UID_KEY);
-
-                CollectionReference notifications = database.collection("Users/"+email+"/Messages");
-                HashMap<String, String> msg = feedback(title, DatetoString, timeStamp);
-                notifications.document(Objects.requireNonNull(timeStamp)).set(msg);
-                Toast.makeText(RequestsAdmin.this, "Notice Rejected.\nFeedback sent to user", Toast.LENGTH_LONG).show();
-            }
-        });
+        loadCommunity();
     }
 
     @Override
@@ -148,6 +108,25 @@ public class RequestsAdmin extends AppCompatActivity {
     }
 
     /**
+     * Retrieves user's community to receive requests.
+     */
+    private void loadCommunity() {
+        DocumentReference userRef = database.document("Users/"+mAuth.getCurrentUser().getEmail());
+        userRef.get()
+                .addOnSuccessListener(documentSnapshot -> {
+                    if(documentSnapshot.exists()){
+                        community = documentSnapshot.getString("Admin Community");
+                        loadRequests(community);
+                    }
+                    else {
+                        startActivity(new Intent(RequestsAdmin.this, Newsfeed.class));
+                        Toast.makeText(RequestsAdmin.this, "No community exists", Toast.LENGTH_LONG).show();
+                    }
+                })
+                .addOnFailureListener(e -> Toast.makeText(RequestsAdmin.this, "Error! "+ e.getMessage(),Toast.LENGTH_SHORT).show());
+    }
+
+    /**
      * Loads notices to be displayed in the general newsfeed
      */
     private void loadRequests(String community) {
@@ -161,14 +140,13 @@ public class RequestsAdmin extends AppCompatActivity {
 
         adapter = new RequestAdapter(options);
         progressDialog.dismiss();
-        RecyclerView requestsRecyclerView = findViewById(R.id.inbox_recyclerView);
         requestsRecyclerView.setHasFixedSize(true);
         requestsRecyclerView.setLayoutManager(new LinearLayoutManager(this));
         requestsRecyclerView.setAdapter(adapter);
-
         adapter.startListening();
+
         /*
-         * Deletes notice from database by using swipe to delete functiom and removes it from recyclerview
+         * Deletes notice from database by using swipe to delete function and removes it from RecyclerView
          */
         new ItemTouchHelper(new ItemTouchHelper.SimpleCallback(0,
                 ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT) {
@@ -180,15 +158,68 @@ public class RequestsAdmin extends AppCompatActivity {
             @Override
             public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
                 adapter.deleteItem(viewHolder.getAdapterPosition());
-                Snackbar.make(relativeLayout, "Notice deleted", Snackbar.LENGTH_LONG).setAction("Action", null).show();
+                Snackbar.make(relativeLayout, "Request deleted", Snackbar.LENGTH_LONG).setAction("Action", null).show();
             }
         }).attachToRecyclerView(requestsRecyclerView);
+
+        adapter.setOnItemClickListener(new RequestAdapter.OnItemClickListener() {
+            String id = String.valueOf(System.currentTimeMillis());
+            //formatting date of feedback
+            Date currentDate = new Date();
+            @SuppressLint("SimpleDateFormat") SimpleDateFormat format = new SimpleDateFormat("d MMM yyyy, HH:mm");
+            String date = format.format(currentDate);
+            @Override
+            public void acceptBtnClick(DocumentSnapshot documentSnapshot, int position) {
+                String category = documentSnapshot.getString(CATEGORY_KEY);
+                String DatetoString = documentSnapshot.getString(DATE_KEY);
+                String body = documentSnapshot.getString(DESCRIPTION_KEY);
+                String timeStamp = documentSnapshot.getString(ID_KEY);
+                String image = documentSnapshot.getString(IMAGE_KEY);
+                String title = documentSnapshot.getString(TITLE_KEY);
+                String name = documentSnapshot.getString(USERNAME_KEY);
+                String communities = documentSnapshot.getString(COMMUNITY_KEY);
+                String email = documentSnapshot.getString(UID_KEY);
+                CollectionReference notifications = database.collection("Users/"+email+"/Messages");
+
+                HashMap<String, String> hashMap = new HashMap<>();
+                hashMap.put(CATEGORY_KEY, category);
+                hashMap.put(DATE_KEY, DatetoString);
+                hashMap.put(DESCRIPTION_KEY, body);
+                hashMap.put(ID_KEY, timeStamp);
+                hashMap.put(IMAGE_KEY, image);
+                hashMap.put(TITLE_KEY, title);
+                hashMap.put(USERNAME_KEY, name);
+                hashMap.put(COMMUNITY_KEY, communities);
+                hashMap.put(UID_KEY, email);
+
+                CollectionReference notices = database.collection("Notices");
+                notices.document(Objects.requireNonNull(timeStamp)).set(hashMap)
+                        .addOnSuccessListener(aVoid -> {
+                            HashMap<String, String> msg = feedback(title, date, id, true);
+                            notifications.document(id).set(msg);
+                            Toast.makeText(RequestsAdmin.this, "Notice posted to newsfeed.\nFeedback sent to user", Toast.LENGTH_LONG).show();
+                        })
+                        .addOnFailureListener(e -> Toast.makeText(RequestsAdmin.this, "Error! "+e.getMessage(), Toast.LENGTH_LONG).show());
+            }
+
+            @Override
+            public void rejectBtnClick(DocumentSnapshot documentSnapshot, int position) {
+                String title = documentSnapshot.getString(TITLE_KEY);
+                String email = documentSnapshot.getString(UID_KEY);
+
+                CollectionReference notifications = database.collection("Users/"+email+"/Messages");
+                HashMap<String, String> msg = feedback(title, date, id, false);
+                notifications.document(Objects.requireNonNull(id)).set(msg);
+                Toast.makeText(RequestsAdmin.this, "Notice Rejected.\nFeedback sent to user", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 
-    private HashMap<String, String> feedback(String title, String DatetoString, String timeStamp){
+    private HashMap<String, String> feedback(String title, String DatetoString, String timeStamp, boolean approve){
         HashMap<String, String> msg = new HashMap<>();
         Notification notification = new Notification(title, DatetoString);
-        notification.approve();
+        if (approve) notification.approve();
+        else notification.reject();
         msg.put("Title", title);
         msg.put("Date", DatetoString);
         msg.put("Status", notification.getStatus());
